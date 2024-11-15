84fe29126bc36753ab1f10a37815eabb132453fe
Rachit Bansal <rachitbansal2500@gmail.com>
2024-11-15 14:37:05 -0500

    wandb config flags

--- scripts/llm/custom_moe.py
+++ scripts/llm/custom_moe.py
@@ -495,6 +495,31 @@ def main() -> None:
     pretrain.log.ckpt.train_time_interval = None
     pretrain.log.ckpt.every_n_train_steps = args.every_n_train_steps
 
+    pretrain.log.wandb.config = {
+        "model_config": args.model_config,
+        "dataset_name": args.dataset_name,
+        "tokenizer_type": args.tokenizer_type,
+        "num_moe_experts": args.num_moe_experts,
+        "num_layers": args.num_layers,
+        "hidden_size": args.hidden_size,
+        "num_attention_heads": args.num_attention_heads,
+        "ffn_hidden_size": args.ffn_hidden_size,
+        "seq_length": args.seq_length,
+        "max_position_embeddings": args.max_position_embeddings,
+        "learning_rate": args.learning_rate,
+        "global_batch_size": args.global_batch_size,
+        "micro_batch_size": args.micro_batch_size,
+        "max_steps": args.max_steps,
+        "tensor_model_parallel_size": args.tensor_model_parallel_size,
+        "pipeline_model_parallel_size": args.pipeline_model_parallel_size,
+        "virtual_pipeline_model_parallel_size": args.virtual_pipeline_model_parallel_size,
+        "context_parallel_size": args.context_parallel_size,
+        "sequence_parallel": args.sequence_parallel,
+        "expert_model_parallel_size": args.expert_model_parallel_size,
+        "num_nodes": args.num_nodes,
+        "num_gpus_per_node": args.num_gpus_per_node
+    }
+
     # Configure training parameters
     pretrain.trainer.max_steps = args.max_steps
     pretrain.optim.config.lr = args.learning_rate
